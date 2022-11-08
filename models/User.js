a434af518185cb0ccb3c2eaba98acf885fc6c634
Dylan Crowley <dcrowdev1025@gmail.com>
2022-11-08 08:55:16 -0500

    initial Post model config

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes} = require('sequelize');
 const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection')
+const sequelize = require('../config/connection');
 
 class User extends Model {
     checkPassword(loginPW) {
@@ -18,7 +18,8 @@ User.init(
         },
         username: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            isUnique: true
         },
         password: {
             type: DataTypes.STRING,
