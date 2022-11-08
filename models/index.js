cc388badd99e3e1018dccc197051ee4203f3d6df
Dylan Crowley <dcrowdev1025@gmail.com>
2022-11-08 09:05:45 -0500

    initial model relationship definitions

--- models/index.js
+++ models/index.js
@@ -3,5 +3,28 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 //define model relationships
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    through: Post
+});
+
+Comment.belongsTo(User, {
+    through: Post
+});
+
+Post.hasMany(Comment, {
+    through: User
+});
+
+Comment.belongsTo(Post, {
+    through: User
+});
 
 module.exports = { User, Post, Comment }
