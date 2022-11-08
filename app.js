1a8f32caf01354c0bb8fb3e17a195ffec5e375c2
Dylan Crowley <dcrowdev1025@gmail.com>
2022-11-08 15:48:26 -0500

    added session to app.js

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require('express');
-const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers')
 const app = express();
+const session = require('express-session');
 
 //PORT
 const PORT = process.env.PORT || 3001;
+
+//sequelize
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict'
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 //handlebars
 const hbs = exphbs.create({});
