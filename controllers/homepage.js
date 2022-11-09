ba6e21250a5ac42ccf4acce5c073656ad611454f
Dylan Crowley <dcrowdev1025@gmail.com>
2022-11-08 21:15:01 -0500

    added seed structure

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,1 +1,7 @@
 const router = require('express').Router;
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    res.render('all-posts', )
+})
