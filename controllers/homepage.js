0c6c60a36a3c19f5ad3296b83c686ba9a8374708
Dylan Crowley <dcrowdev1025@gmail.com>
2022-11-09 07:30:52 -0500

    able to get data to show through views

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,7 +1,11 @@
-const router = require('express').Router;
+const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
-router.get('/', (req, res) => {
-    res.render('all-posts', )
-})
+router.get('/', async (req, res) => {
+    const postData = await Post.findAll();
+    const posts = postData.map((post) => post.get({ plain: true }))
+    res.render('all-posts', { posts })
+    });
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
+// const apiRoutes = require('./api');
 const homeRoutes = require('./homepage');
-const dashboardRoutes = require('./dashboard');
+// const dashboardRoutes = require('./dashboard');
 
 
-router.use('/api', apiRoutes);
+// router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
+// router.use('/dashboard', dashboardRoutes);
 
 
 module.exports = router;
